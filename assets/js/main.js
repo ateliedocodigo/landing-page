8508a7820fee253d6e712baaead889c1b6af6069
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-10-06 12:45:25 -0300

    Fix catch submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-    "use strict";
+    'use strict';
 
     // Methods/polyfills.
 
@@ -43,57 +43,58 @@
         };
 
         // Vars.
-            var pos = 0, lastPos = 0,
-                $wrapper, $bgs = [], $bg,
-                k, v;
+        var pos = 0, lastPos = 0,
+            $wrapper, $bgs = [], $bg,
+            k, v;
 
         // Create BG wrapper, BGs.
-            $wrapper = document.createElement('div');
-                $wrapper.id = 'bg';
-                $body.appendChild($wrapper);
+        $wrapper = document.createElement('div');
+        $wrapper.id = 'bg';
+        $body.appendChild($wrapper);
 
-            for (k in settings.images) {
+        for (k in settings.images) {
 
-                // Create BG.
-                    $bg = document.createElement('div');
-                        $bg.style.backgroundImage = 'url("' + k + '")';
-                        $bg.style.backgroundPosition = settings.images[k];
-                        $wrapper.appendChild($bg);
+            // Create BG.
+            $bg = document.createElement('div');
+            $bg.style.backgroundImage = 'url("' + k + '")';
+            $bg.style.backgroundPosition = settings.images[k];
+            $wrapper.appendChild($bg);
 
-                // Add it to array.
-                    $bgs.push($bg);
+            // Add it to array.
+            $bgs.push($bg);
 
-            }
+        }
 
         // Main loop.
+        $bgs[pos].classList.add('visible');
+        $bgs[pos].classList.add('top');
+
+        // Bail if we only have a single BG or the client doesn't support transitions.
+        if ($bgs.length == 1 || !canUse('transition')) {
+            return;
+        }
+
+        window.setInterval(function() {
+
+            lastPos = pos;
+            pos++;
+
+            // Wrap to beginning if necessary.
+            if (pos >= $bgs.length) {
+                pos = 0;
+            }
+
+            // Swap top images.
+            $bgs[lastPos].classList.remove('top');
             $bgs[pos].classList.add('visible');
             $bgs[pos].classList.add('top');
 
-            // Bail if we only have a single BG or the client doesn't support transitions.
-                if ($bgs.length == 1
-                ||  !canUse('transition'))
-                    return;
+            // Hide last image after a short delay.
+            window.setTimeout(function() {
+                $bgs[lastPos].classList.remove('visible');
+            }, settings.delay / 2);
 
-            window.setInterval(function() {
-
-                lastPos = pos;
-                pos++;
-
-                // Wrap to beginning if necessary.
-                    if (pos >= $bgs.length)
-                        pos = 0;
-
-                // Swap top images.
-                    $bgs[lastPos].classList.remove('top');
-                    $bgs[pos].classList.add('visible');
-                    $bgs[pos].classList.add('top');
-
-                // Hide last image after a short delay.
-                    window.setTimeout(function() {
-                        $bgs[lastPos].classList.remove('visible');
-                    }, settings.delay / 2);
-
-            }, settings.delay);
+        }, settings.delay);
 
     })();
 
@@ -134,8 +135,9 @@
         // Note: If you're *not* using AJAX, get rid of this event listener.
         $form.addEventListener('submit', function(event) {
             try {
+                console.log('submitting form');
                 ga('send', 'event', 'List', 'subscribe', 'Capa ateliedocodigo');
-            } catch(){}
+            } catch (e) {}
         });
     })();
 })();
